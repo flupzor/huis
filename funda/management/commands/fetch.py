e254031234c74e8d864c12500ea6e7ea615cc738
Alexander  Schrijver <alex@flupzor.nl>
2017-07-23 16:58:29 +0200

    Save new 'woningen' in the reversion context. (thus creating a revision)

--- funda/management/commands/fetch.py
+++ funda/management/commands/fetch.py
@@ -1,8 +1,11 @@
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 
+import reversion
+
 from ...models import Woning
 from ...funda import Funda
+
 
 class Command(BaseCommand):
     help = 'Retrieve woningen'
@@ -11,10 +14,11 @@ class Command(BaseCommand):
         def interesting(obj):
             return not obj.IsVerkocht
 
-        existing_objects = [{'Id': guid} for guid in Woning.objects.values_list("guid", flat=True)]
-        funda = Funda(api_key=settings.FUNDA_API_KEY, query=settings.FUNDA_QUERY,
-                      existing_objects=existing_objects)
-        funda_woningen = [w for w in funda if interesting(w)]
-        Woning.objects.from_funda_woning(*funda_woningen)
-        self.stdout.write(self.style.SUCCESS('Found {} unlisted woningen '.format(len(funda.unlisted_objs))))
-        self.stdout.write(self.style.SUCCESS('Processed {} woningen '.format(len(funda_woningen))))
+        with reversion.create_revision():
+            existing_objects = [{'Id': guid} for guid in Woning.objects.values_list("guid", flat=True)]
+            funda = Funda(api_key=settings.FUNDA_API_KEY, query=settings.FUNDA_QUERY,
+                          existing_objects=existing_objects)
+            funda_woningen = [w for w in funda if interesting(w)]
+            Woning.objects.from_funda_woning(*funda_woningen)
+            self.stdout.write(self.style.SUCCESS('Found {} unlisted woningen '.format(len(funda.unlisted_objs))))
+            self.stdout.write(self.style.SUCCESS('Processed {} woningen '.format(len(funda_woningen))))
